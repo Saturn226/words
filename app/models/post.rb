074dc0a722b3f396d458269dbbc1126532c7626b
Scotty Runyan <runyans7@gmail.com>
2016-01-25 10:41:27 -0500

    Associate User to posts
    Users are now associated to a post by the alias Author. post.author returns User associated to the Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
 
+  belongs_to :author, class: 'User', foreign_key: 'user_id'
 end
